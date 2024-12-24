4480491bc6ac5764476a93392deadf90d4d15f16
Rakesh3993 <kr.rakesh3993@gmail.com>
2024-12-24 10:19:24 +0530

    Nav Title chnaged

--- Assignment/CelebSearch/Constants/AppConstants.swift
+++ Assignment/CelebSearch/Constants/AppConstants.swift
@@ -11,7 +11,7 @@ struct AppConstants {
     static let baseURL = "https://api.themoviedb.org"
     static let API_KEY = "697d439ac993538da4e3e60b54e762cd"
     static let searchBarPlaceHolderText = "Search for Actors, Directors"
-    static let navTitle = "Celebrity"
+    static let navTitle = "Stardom"
 }
 
 struct identifier {
